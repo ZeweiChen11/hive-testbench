c7992a7fcd0a6cf45e1240d0c9539de7cc88d1a0
ZeweiChen11 <zewei.chen@intel.com>
2018-08-06 14:53:49 +0800

    update running script

--- run_tpch_kudu.sh
+++ run_tpch_kudu.sh
@@ -1,13 +1,15 @@
 #/bin/sh
 set -e
-SCALE=1800
+SCALE=2
 DIR=/user/root/tpch/
-cor_node=atlas-cdh95.jf.intel.com
+cor_node=atlas-cdh96.jf.intel.com
 FILE_FORMAT="TEXTFILE"
+IMPALA_HOME="/root/workspace/impala/"
+IMPALA_SHELL="${IMPALA_HOME}/bin/impala-shell.sh"
 #time=`date +%Y%m%d%H%M%S`
 #LOG_DIR=kudu_log_${time}
 #if [ ! -d ${LOG_DIR} ]; then
-#	mkdir -p ${LOG_DIR}
+#       mkdir -p ${LOG_DIR}
 #fi
 
 IMPALA_DB_NAME=tpch_impala_${SCALE}
@@ -21,54 +23,50 @@ function getExecTime() {
 }
 
 function Load(){
-	echo "Dropping caches..."
-	echo 3 > /proc/sys/vm/drop_caches
-	echo "Loading text data into external tables."
-	impala-shell -i ${cor_node} -q "create database if not exists ${IMPALA_DB_NAME}"
-	impala-shell -i ${cor_node} -q "create database if not exists ${KUDU_DB_NAME}"
-	for t in lineitem customer nation orders part partsupp region supplier
-#	for t in lineitem
-	do
-		echo 3 > /proc/sys/vm/drop_caches
-		echo "Start loading ${t} ..." 2>&1 | tee -a ${LOG_DIR}/load.log
-		date 2>&1 | tee -a ${LOG_DIR}/load.log
-		impala-shell -i ${cor_node} -f tpch-create-table/${t}.sql -d ${IMPALA_DB_NAME} --var=DB=${IMPALA_DB_NAME} --var=LOCATION=${DIR}/${SCALE} --var=KUDU_DB_NAME=${KUDU_DB_NAME} --var=FILE_FORMAT=${FILE_FORMAT} --query_option=DEFAULT_SPILLABLE_BUFFER_SIZE=1MB 2>&1 | tee -a ${LOG_DIR}/load.log
-		date 2>&1 | tee -a ${LOG_DIR}/load.log
-		echo "End loading ${t} ..." 2>&1 | tee -a ${LOG_DIR}/load.log
-	done
-	echo "Loading done!"
+        echo "Dropping caches..."
+        echo 3 > /proc/sys/vm/drop_caches
+        echo "Loading text data into external tables."
+        ${IMPALA_SHELL} -i ${cor_node} -q "create database if not exists ${IMPALA_DB_NAME}"
+        ${IMPALA_SHELL} -i ${cor_node} -q "create database if not exists ${KUDU_DB_NAME}"
+#       for t in lineitem customer nation orders part partsupp region supplier
+        for t in nation
+        do
+                echo 3 > /proc/sys/vm/drop_caches
+                echo "Start loading ${t} ..." 2>&1 | tee -a ${LOG_DIR}/load.log
+                date 2>&1 | tee -a ${LOG_DIR}/load.log
+                ${IMPALA_SHELL} -i ${cor_node} -f tpch-create-table/${t}.sql -d ${IMPALA_DB_NAME} --var=DB=${IMPALA_DB_NAME} --var=LOCATION=${DIR}/${SCALE} --var=KUDU_DB_NAME=${KUDU_DB_NAME} --var=FILE_FORMAT=${FILE_FORMAT} --query_option=DEFAULT_SPILLABLE_BUFFER_SIZE=1MB 2>&1 | tee -a ${LOG_DIR}/load.log
+                date 2>&1 | tee -a ${LOG_DIR}/load.log
+                echo "End loading ${t} ..." 2>&1 | tee -a ${LOG_DIR}/load.log
+        done
+        echo "Loading done!"
 }
 
 function runQuery(){
-	query=$1
-	stream=$2
-	echo "run query ${query} in ${stream}..." 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
-	start=$(date +%s%3N)
-	date 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
-	impala-shell -i ${cor_node} -f sample-queries-tpch/tpch_query${query}.sql -d ${KUDU_DB_NAME} -p --query_option=DEFAULT_SPILLABLE_BUFFER_SIZE=1MB 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
-	#impala-shell -i ${cor_node} -f sample-queries-tpch/tpch_query${query}.sql -d ${KUDU_DB_NAME} -p 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
-	end=$(date +%s%3N)
-	getExecTime $start $end >> ${LOG_DIR}/tpch_query${query}_${stream}.log
-	date 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
-	echo "query ${query} done in ${stream} !" >> ${LOG_DIR}/tpch_query${query}_${stream}.log
+        query=$1
+        stream=$2
+        echo "run query ${query} in ${stream}..." 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
+        start=$(date +%s%3N)
+        date 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
+        ${IMPALA_SHELL} -i ${cor_node} -f sample-queries-tpch/tpch_query${query}.sql -d ${KUDU_DB_NAME} -p --query_option=DEFAULT_SPILLABLE_BUFFER_SIZE=1MB 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
+        #${IMPALA_SHELL} -i ${cor_node} -f sample-queries-tpch/tpch_query${query}.sql -d ${KUDU_DB_NAME} -p 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
+        end=$(date +%s%3N)
+        getExecTime $start $end >> ${LOG_DIR}/tpch_query${query}_${stream}.log
+        date 2>&1 | tee -a ${LOG_DIR}/tpch_query${query}_${stream}.log
+        echo "query ${query} done in ${stream} !" >> ${LOG_DIR}/tpch_query${query}_${stream}.log
 }
 
 function runStream(){
-	stream_nu=$1
-	echo "run stream ${stream_nu}..." 2>&1 | tee -a ${LOG_DIR}/stream_${stream_nu}.log
-	start=$(date +%s%3N)
+        stream_nu=$1
+        echo "run stream ${stream_nu}..." 2>&1 | tee -a ${LOG_DIR}/stream_${stream_nu}.log
+        start=$(date +%s%3N)
         date 2>&1 | tee -a ${LOG_DIR}/stream_${stream_nu}.log
-	impala-shell -i ${cor_node} -f stream_query/s${stream_nu}.sql -d ${KUDU_DB_NAME} 2>&1 | tee -a ${LOG_DIR}/stream_${stream_nu}.log
-	end=$(date +%s%3N)
+        ${IMPALA_SHELL} -i ${cor_node} -f stream_query/s${stream_nu}.sql -d ${KUDU_DB_NAME} 2>&1 | tee -a ${LOG_DIR}/stream_${stream_nu}.log
+        end=$(date +%s%3N)
         getExecTime $start $end 2>&1 | tee -a  ${LOG_DIR}/stream_${stream_nu}.log
-	date 2>&1 | tee -a ${LOG_DIR}/stream_${stream_nu}.log
-	echo "Stream ${stream_nu} done!" 2>&1 | tee -a  ${LOG_DIR}/stream_${stream_nu}.log
+        date 2>&1 | tee -a ${LOG_DIR}/stream_${stream_nu}.log
+        echo "Stream ${stream_nu} done!" 2>&1 | tee -a  ${LOG_DIR}/stream_${stream_nu}.log
 }
 
-function Read(){
-	echo "Start reading..."
-	impala-shell -i ${cor_node} -f tpch-create-table/read.sql -d ${KUDU_DB_NAME} >> ${LOG_DIR}/table.log
-}
 #Load
 
 echo "Dropping caches..."
@@ -113,8 +111,8 @@ Load
 #
 #for sq in ${stream_query}
 #do
-#	runQuery ${sq} ${stream_nu} &
-#	let stream_nu+=1
+#       runQuery ${sq} ${stream_nu} &
+#       let stream_nu+=1
 #done
 #
 #wait
@@ -149,12 +147,12 @@ Load
 #
 #for n in `seq 1 22`
 #do
-#	echo "===start run query $n==="
-#	date
-#	echo 3 > /proc/sys/vm/drop_caches
-#	runQuery ${n} 0
-#	date
-#	echo "===end query $n ==="
+#       echo "===start run query $n==="
+#       date
+#       echo 3 > /proc/sys/vm/drop_caches
+#       runQuery ${n} 0
+#       date
+#       echo "===end query $n ==="
 #done
 #
 #echo 3 > /proc/sys/vm/drop_caches
@@ -174,3 +172,4 @@ Load
 #        echo "===end query $n ==="
 #done
 #=========================
+
