7123226f2323d75020ce42862c8939101db99ba5
ZeweiChen11 <zewei.chen@intel.com>
2018-09-26 15:07:32 +0800

    fix error in q4

--- kudu-spark-tools/sample-queries-tpch/tpch_query4.sql
+++ kudu-spark-tools/sample-queries-tpch/tpch_query4.sql
@@ -2,7 +2,7 @@ select
 	orders._c5,
 	count(*) as order_count
 from
-	orders as o
+	orders
 where
 	orders._c4 >= '1996-05-01'
 	and orders._c4 < '1996-08-01'
@@ -12,7 +12,7 @@ where
 		from
 			lineitem
 		where
-			lineitem._c0 = o.orders._c0
+			lineitem._c0 = orders._c0
 			and lineitem._c11 < lineitem._c12
 	)
 group by
