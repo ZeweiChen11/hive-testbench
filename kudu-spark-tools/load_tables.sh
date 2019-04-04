a6862d791700131b1ed1ab1b3b6ef8c1dc69bc61
zewei <zewei.chen@intel.com>
2019-04-04 20:45:57 +0800

    Update tpch load kudu script

--- kudu-spark-tools/load_tables.sh
+++ kudu-spark-tools/load_tables.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This is to load TPC-H tables into kudu using spark-submit
 # Before executing this script, please build kudu-spark-tools in kudu project.
+#   git clone https://github.com/ZeweiChen11/kudu.git
 #   cd kudu/java/kudu-spark-tools
 #   mvn clean package -DskipTests
 #
@@ -17,6 +18,14 @@ KUDU_HOME=/home/bduser/kudu/
 #SPARK_SUBMIT=/root/zewei/spark/bin/spark-submit
 SPARK_SUBMIT=spark-submit
 IMPALA_VERSION=cdh #or "apache"
+
+if [ ! -f ${KUDU_HOME}/java/kudu-spark-tools/target/kudu-spark2-tools_2.11-1.5.0.jar ]; then
+        echo "Please build the kudu-spark-tools at first!"
+	echo "git clone https://github.com/ZeweiChen11/kudu.git"
+	echo "cd kudu/java/kudu-spark-tools"
+	echo "mvn clean package -DskipTests"
+        exit 1
+fi
 
 time=`date +%Y%m%d%H%M%S`
 LOG_DIR=kudu_log_${time}
@@ -49,38 +58,28 @@ function load_table {
     --path=${data} --table-name="impala::tpch_kudu_${SCALE}.${t}" --table="${t}" 2>&1 | tee -a ${LOG_DIR}/load_${t}.log
 }
 
+function load(){
+	#customer part partsupp lineitem suppliers
+	other_table=$1
+	${IMPALA_SHELL} -f tpch-create-table/${other_table}.sql -d ${KUDU_DB_NAME} --var=KUDU_DB_NAME=${KUDU_DB_NAME}
+	table_file_number=`hadoop fs -ls ${DIR}/${SCALE}/${other_table} | wc -l`
+	for i in `seq 0 $table_file_number`
+	do
+	    file_num=`printf "%05d\n" ${i}`
+	    load_table customer ${DIR}/${SCALE}/${other_table}/data-m-${file_num}
+	done
+}
+
+#load nation region supplier
 for table in nation region supplier
 do
     ${IMPALA_SHELL} -f tpch-create-table/${table}.sql -d ${KUDU_DB_NAME} --var=KUDU_DB_NAME=${KUDU_DB_NAME}
-    load_table ${table} /user/root/tpch/${SCALE}/${table}/data-m-*
+    load_table ${table} ${DIR}/${SCALE}/${table}/data-m-*
 done
 
-${IMPALA_SHELL} -f tpch-create-table/customer.sql -d ${KUDU_DB_NAME} --var=KUDU_DB_NAME=${KUDU_DB_NAME}
-for i in {0..1}
+#load customer part partsupp lineitem suppliers
+for t in customer part partsupp lineitem suppliers
 do
-    load_table customer /user/root/tpch/${SCALE}/customer/data-m-0${i}*
-done
-
-${IMPALA_SHELL} -f tpch-create-table/part.sql -d ${KUDU_DB_NAME} --var=KUDU_DB_NAME=${KUDU_DB_NAME}
-for i in {0..1}
-do
-    load_table part /user/root/tpch/${SCALE}/part/data-m-0${i}*
-done
-
-${IMPALA_SHELL} -f tpch-create-table/partsupp.sql -d ${KUDU_DB_NAME} --var=KUDU_DB_NAME=${KUDU_DB_NAME}
-for i in {00..19}
-do
-    load_table partsupp /user/root/tpch/${SCALE}/partsupp/data-m-0${i}*
-done
-
-${IMPALA_SHELL} -f tpch-create-table/orders.sql -d ${KUDU_DB_NAME} --var=KUDU_DB_NAME=${KUDU_DB_NAME}
-for i in {00..19}
-do
-    load_table orders /user/root/tpch/${SCALE}/orders/data-m-0${i}*
-done
-
-${IMPALA_SHELL} -f tpch-create-table/lineitem.sql -d ${KUDU_DB_NAME} --var=KUDU_DB_NAME=${KUDU_DB_NAME}
-for i in {000..199}
-do
-    load_table lineitem /user/root/tpch/${SCALE}/lineitem/data-m-0${i}*
-done
+	echo "Loading table ${t}..."
+	load ${t}
+done	
